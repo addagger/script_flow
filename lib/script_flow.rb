270899c167e4af7fd35c0ca0ab4b4573490f0e2c
Valery Kvon <addagger@gmail.com>
2013-01-25 22:39:28 +0400

    moved to "active_tools"

--- lib/script_flow.rb
+++ lib/script_flow.rb
@@ -4,6 +4,14 @@ module ScriptFlow
   def self.load!
     require 'script_flow/engine'
     require 'script_flow/railtie'
+    warn <<-EOW
+
+    DEPRECATION WARNING: Gem 'script_flow' has been moved into 'active_tools' gem.
+    Further development of 'script_flow' will no longer continue.
+
+    Thank you!
+
+EOW
   end
 end
 
--- lib/script_flow/version.rb
+++ lib/script_flow/version.rb
@@ -1,3 +1,3 @@
 module ScriptFlow
-  VERSION = "0.0.3"
+  VERSION = "0.0.4"
 end
